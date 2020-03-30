0124687b612a0f3a578f440623536741fcb499fd
Neja <nejira.vrana@yahoo.com>
2020-03-30 14:43:08 +0200

    final task solution

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -1,10 +1,34 @@
 package sgh;
 
-public class PrimeNumbers {
+public class  PrimeNumbers {
+
+    public static boolean isPrime(int x){
+        for(int i=2;i<x;i++){
+            if(x%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void primes(int n) {
-        // Your code here
-        System.out.println("2, 3, 5, 7");
+        int [] array =new int [n];
+        int a=0;
+        int x=2;
+        while(a<n){
+            if(isPrime(x)){
+                array[a]=x;
+                a++;
+
+            }
+            x++;
+        }
+        for(int i=0;i<n;i++) {
+            if(i==n-1)
+            System.out.printf(array[i] + "\n");
+            else
+            System.out.print(array[i] + ", ");
+        }
     }
 
 
